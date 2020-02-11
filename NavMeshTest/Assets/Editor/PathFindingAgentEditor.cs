8b81dd4b38e1f63eabfb9d83ec0c751cbfc17893
大胆包拯 <2295534490@qq.com>
2020-02-11 19:07:50 +0800

    NavMeshTest

---
+++ NavMeshTest/Assets/Editor/PathFindingAgentEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathFindingAgent))]
+public class PathFindingAgentEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PathFindingAgent pathFindingAgent = (PathFindingAgent)target;
+
+        if( GUILayout.Button("pathFindingAgent"))
+        {
+            pathFindingAgent.GoToGarget();
+        }
+    }
+}
---
+++ NavMeshTest/Assets/PathFindingAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathFindingAgent : MonoBehaviour
+{
+    public GameObject target;
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void GoToGarget()
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+    }
+}
